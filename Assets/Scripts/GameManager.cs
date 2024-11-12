564b6bf6efe88c611b9c0032d547b8ff8e2682e2
ViliusKk <vilius.kuokstas@gmail.com>
2024-11-12 19:42:39 +0200

    Cursor and 2X upgrades

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,20 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int clickPower = 1;
+    float clicks;
+    float cps;
+
     public TMP_Text clicksText;
+    public TMP_Text cpsText;
+
+    public Button employeeButton;
     public TMP_Text employeeCountText;
     public TMP_Text employeePriceText;
-    public TMP_Text cpsText;
-    int employeeCount;
-    int clicks;
     public int employeePrice;
+    int employeeCount;
+
+    public Button cursorButton;
+    public TMP_Text cursorCountText;
+    public TMP_Text cursorPriceText;
+    public int cursorPrice;
+    int cursorCount;
+
+    public Button upgradeButton;
 
     public float clickTimer;
     public float autoSaveTimer;
-    float cps;
 
     void Start()
     {
@@ -28,8 +41,9 @@ public class GameManager : MonoBehaviour
         
         clicksText.text = clicks.ToString("N0");               //       Formatting of 1054.32179:
         employeeCountText.text = employeeCount.ToString();     //          N:                     1,054.32
-        employeePriceText.text = $"Price: {employeePrice}";    //          N0:                    1,054 
-                                                               //          N1:                    1,054.3
+        employeePriceText.text = $"Price: {employeePrice}";    //          N0:                    1,054
+        cursorCountText.text = cursorCount.ToString();         //          N1:                    1,054.3
+        cursorPriceText.text = $"Price: {cursorPrice}";
         clickTimer += Time.deltaTime;                          //          N2:                    1,054.32
         autoSaveTimer += Time.deltaTime;                       //          N3:                    1,054.322
 
@@ -38,27 +52,42 @@ public class GameManager : MonoBehaviour
         {
             clickTimer = 0;
             UpdateCPS();
-            clicks += Convert.ToInt32(cps);
+            clicks += cps;
         }
         if (autoSaveTimer >= 10)
         {
             Save();
             autoSaveTimer = 0;
         }
+
+
+        // hiding or showing buttons, depends if they can be bought
+        employeeButton.interactable = clicks >= employeePrice;
+        cursorButton.interactable = clicks >= cursorPrice;
+        upgradeButton.interactable = clicks >= 100;
+
     }
 
     void Save()
     {
-        PlayerPrefs.SetInt("clicks", clicks);
+        PlayerPrefs.SetFloat("clicks", clicks);
         PlayerPrefs.SetInt("employee", employeeCount);
         PlayerPrefs.SetInt("employeePrice", employeePrice);
+        PlayerPrefs.SetFloat("cps", cps);
+        PlayerPrefs.SetInt("cursor", cursorCount);
+        PlayerPrefs.SetInt("cursorPrice", cursorPrice);
+        PlayerPrefs.SetInt("clickPower", clickPower);
     }
 
     void Load()
     {
-        clicks = PlayerPrefs.GetInt("clicks");
+        clicks = PlayerPrefs.GetFloat("clicks");
         employeeCount = PlayerPrefs.GetInt("employee");
         employeePrice = PlayerPrefs.GetInt("employeePrice", employeePrice);
+        cps = PlayerPrefs.GetFloat("cps", cps);
+        cursorCount = PlayerPrefs.GetInt("cursor", cursorCount);
+        cursorPrice = PlayerPrefs.GetInt("cursorPrice", cursorPrice);
+        clickPower = PlayerPrefs.GetInt("clickPower", clickPower);
     }
 
     public void BuyEmployee()
@@ -69,16 +98,35 @@ public class GameManager : MonoBehaviour
             employeeCountText.text = employeeCount.ToString();
 
             clicks -= employeePrice;
-            clicksText.text = clicks.ToString();
+            clicksText.text = clicks.ToString("N0");
 
             employeePrice += employeePrice / 10;
             employeePriceText.text = $"Price: {employeePrice}";
+
+            cps++;
+        }
+    }
+
+    public void BuyCursor()
+    {
+        if (clicks >= cursorPrice)
+        {
+            cursorCount++;
+            cursorCountText.text = cursorCount.ToString();
+
+            clicks -= cursorPrice;
+            clicksText.text = clicks.ToString("N0");
+
+            cursorPrice += cursorPrice / 10;
+            cursorPriceText.text = $"Price: {cursorPrice}";
+
+            cps += 0.1f;
         }
     }
 
     public void AddClick()
     {
-        clicks++;
+        clicks += clickPower;
     }
 
     void OnApplicationQuit()
@@ -88,7 +136,13 @@ public class GameManager : MonoBehaviour
 
     void UpdateCPS() // used to calculate cps from all purchased upgrades
     {
-        cps = employeeCount;
-        cpsText.text = $"{cps} cps";
+        //cps = employeeCount;
+        cpsText.text = $"{cps.ToString("N1")} cps";
+    }
+
+    public void BuyClickUpgrade()
+    {
+        clickPower *= 2;
+        clicks -= 100;
     }
 }
