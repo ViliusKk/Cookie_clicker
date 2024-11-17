43b750cc2aa5317a5e354e5157efd9fa2baaff2f
ViliusKk <vilius.kuokstas@gmail.com>
2024-11-17 14:13:22 +0200

    cookie click animation

--- Assets/Scripts/Cookie.cs
+++ Assets/Scripts/Cookie.cs
@@ -7,8 +7,17 @@ public class Cookie : MonoBehaviour
 {
     public UnityEvent onClick;
 
+    void Update()
+    {
+        if (transform.localScale.x > 1f)
+        {
+            transform.localScale -= Vector3.one * 2 * Time.deltaTime;
+        }
+    }
+
     void OnMouseDown()
     {
+        transform.localScale = Vector3.one * 1.2f;
         onClick.Invoke();
     }
 }
